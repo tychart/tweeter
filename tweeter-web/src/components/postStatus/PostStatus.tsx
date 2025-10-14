a343560f2cc6038152c1e9e217f2b8972dae9871
Tyler Chartrand <tyler@thechartrands.com>
2025-10-13 23:55:53 -0600

    Abstracted PostStatus to PostStatusPresenter because I didn't see it during milestone 2a

--- tweeter-web/src/components/postStatus/PostStatus.tsx
+++ tweeter-web/src/components/postStatus/PostStatus.tsx
@@ -1,50 +1,29 @@
 import "./PostStatus.css";
-import { useState } from "react";
-import { AuthToken, Status } from "tweeter-shared";
+import { useRef, useState } from "react";
 import { useMessageActions } from "../toaster/MessageHooks";
 import { useUserInfo } from "../userInfo/UserInfoHooks";
+import { MessageView } from "../../presenter/Presenter";
+import { PostStatusPresenter } from "../../presenter/PostStatusPresenter";
 
 const PostStatus = () => {
   const { displayInfoMessage, displayErrorMessage, deleteMessage } =
     useMessageActions();
-
   const { currentUser, authToken } = useUserInfo();
   const [post, setPost] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+
+  const listener: MessageView = {
+    displayErrorMessage: displayErrorMessage,
+    displayInfoMessage: displayInfoMessage,
+    deleteMessage: deleteMessage,
+  };
+
+  const presenterRef = useRef<PostStatusPresenter>(
+    new PostStatusPresenter(listener)
+  );
 
   const submitPost = async (event: React.MouseEvent) => {
     event.preventDefault();
-
-    var postingStatusToastId = "";
-
-    try {
-      setIsLoading(true);
-      postingStatusToastId = displayInfoMessage("Posting status...", 0);
-
-      const status = new Status(post, currentUser!, Date.now());
-
-      await postStatus(authToken!, status);
-
-      setPost("");
-      displayInfoMessage("Status posted!", 2000);
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to post the status because of exception: ${error}`
-      );
-    } finally {
-      deleteMessage(postingStatusToastId);
-      setIsLoading(false);
-    }
-  };
-
-  const postStatus = async (
-    authToken: AuthToken,
-    newStatus: Status
-  ): Promise<void> => {
-    // Pause so we can see the logging out message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
-
-    // TODO: Call the server to post the status
+    presenterRef.current.submitPost(authToken, currentUser, post, setPost);
   };
 
   const clearPost = (event: React.MouseEvent) => {
@@ -79,7 +58,7 @@ const PostStatus = () => {
           style={{ width: "8em" }}
           onClick={submitPost}
         >
-          {isLoading ? (
+          {presenterRef.current.isLoading ? (
             <span
               className="spinner-border spinner-border-sm"
               role="status"
--- tweeter-web/src/presenter/PostStatusPresenter.ts
+++ tweeter-web/src/presenter/PostStatusPresenter.ts
@@ -1,11 +1,55 @@
-import { AuthToken } from "tweeter-shared";
-import { StoryService } from "../model.service/StatusItemService";
-import { StatusItemPresenter, StatusItemView } from "./StatusItemPresenter";
+import { Status, AuthToken, User } from "tweeter-shared";
+import { MessageView, Presenter } from "./Presenter";
 
-export class PostStatusPresenter {
-  private storyService: StoryService;
+export class PostStatusPresenter extends Presenter<MessageView> {
+  private _isLoading: boolean = false;
 
-  public constructor(view: StatusItemView) {
-    this.storyService = new StoryService();
+  public constructor(view: MessageView) {
+    super(view);
+  }
+
+  public get isLoading() {
+    return this._isLoading;
+  }
+
+  public async submitPost(
+    authToken: AuthToken | null,
+    currentUser: User | null,
+    post: string,
+    setPost: (post: string) => void
+  ) {
+    var postingStatusToastId = "";
+
+    try {
+      this._isLoading = true;
+      postingStatusToastId = this.view.displayInfoMessage(
+        "Posting status...",
+        0
+      );
+
+      const status = new Status(post, currentUser!, Date.now());
+
+      await this.postStatus(authToken!, status);
+
+      setPost("");
+      this.view.displayInfoMessage("Status posted!", 2000);
+    } catch (error) {
+      this.view.displayErrorMessage(
+        `Failed to post the status because of exception: ${error}`
+      );
+    } finally {
+      this.view.deleteMessage(postingStatusToastId);
+      this._isLoading = false;
+    }
+  }
+
+  private async postStatus(
+    authToken: AuthToken,
+    newStatus: Status
+  ): Promise<void> {
+    // Pause so we can see the logging out message. Remove when connected to the server
+    await new Promise((f) => setTimeout(f, 2000));
+
+    // TODO: Call the server to post the status
   }
 }
